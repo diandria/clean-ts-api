c47d296b4b805a87e3f9966003a8733f36f450c0
Diandria Xavier <diandriaxavier@gmail.com>
2021-02-22 09:51:26 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { HttpResponse, HttpRequest, Controller, AddAccount, Validation } from './signup-protocols'
-import { badRequest, serverError, ok } from '../../helpers/http-helper'
+import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
 
 export class SingUpController implements Controller {
   private readonly addAccount: AddAccount
